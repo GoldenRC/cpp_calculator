d546abf7bc645eb79115eea37ca2f4d968627fd9
GoldenRC <damian.maciaszek00@gmail.com>
2019-11-05 21:07:51 +0100

    progress

--- cpp_calculator/cpp_calculator.cpp
+++ cpp_calculator/cpp_calculator.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
+
 int main()
 {
-    std::cout << "Hello World!\n";
-	std::cout << "Hello World!\n";
-	std::cout << "Hello World!\n";
-	std::cout << "Hello World!\n"
-		std::cout << "Hello World!\n";
-	std::cout << "Hello World!\n";
+	int n, x, choice;
+		std::cout << "Welcome in my calculator!" << std::endl;
+		std::cout << "Type first number: ";
+		std::cin >> n;
+		std::cout << "Type second number: ";
+		std::cin >> x;
+		std::cout << "What do you want to do?" << std::endl << "[1] add" << std::endl << "[2] substract" << std::endl << "[3] multiply" << std::endl << "[4] divide" << std::endl << "[5] leave program" << std::endl;
+		std::cin >> choice;
 }
